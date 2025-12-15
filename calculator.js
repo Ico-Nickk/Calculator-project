f9dc31c66e2272b515b83a135fe97ebe5b95a00e
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2025-12-15 12:00:43 +0300

    created functions for each mathematical operation and operate function.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,46 @@
+function add(a, b) {
+    return a + b;
+};
 
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    return a/b;
+};
+
+const operations = {
+    "add": add(),
+    "subtract": subtract(),
+    "multiply": multiply(),
+    "divide": divide(),
+};
+
+let num1 = 0; 
+let num2 = 0;
+let operator = NaN;
+
+function operate(num1, num2, operation) {
+    console.log(`${num1}${operator}${num2}`);
+    switch (operation) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default: 
+            console.log(`${operator} is not a valid operation`);
+    };
+};
