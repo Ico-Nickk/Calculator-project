acab170148e21b4737e372ea2c41cc1080588727
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2025-12-17 15:21:11 +0300

    created function to update display when buttons are pressed and display final result of the operation. set buttons to update display with their value.

--- calculator.js
+++ calculator.js
@@ -25,8 +25,8 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
-function operate(num1, num2, operation) {
-    operator = operation;
+function operate(arr) {
+    [num1, operator, num2] = arr;
     switch (operator) {
         case "+":
             return add(num1, num2);
@@ -44,4 +44,28 @@ function operate(num1, num2, operation) {
             console.log(`${operator} is not a valid operation`);
     };
 };
+
+const calcDisplay = document.querySelector("#calcDisplay");
+
+let opperationArr = []
+
+function updateDisplay (val) {
+    opperationArr.push(val)
+    calcDisplay.textContent = calcDisplay.textContent + val;
+    if(opperationArr.length === 3) {
+        console.log(opperationArr);
+        calcDisplay.textContent= operate(opperationArr);
+    };
+
+};
+
+const bttn1 = document.querySelector("#num1");
+bttn1.addEventListener("click", () => updateDisplay(Number(bttn1.value)));
+
+const addBttn = document.querySelector("#addBttn");
+addBttn.addEventListener("click", () => updateDisplay(addBttn.value));
+
+const bttn2 = document.querySelector("#num2");
+bttn2.addEventListener("click", () => updateDisplay(Number(bttn2.value)));
+
 
